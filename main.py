83cfda46a75e0d8b863b0c7c9bccbd2ab5b86cc4
lemon <l3mon@lemons-MacBook-Air.local>
2025-04-23 04:03:33 +0530

    basic prototype done, need to improve pattern matching function

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
+
+
+def check_post(atag):
+        id = atag['href']
+        url = f'https://pastebin.com/{id}'
+        response = requests.get(url)
+        raw_content = response.text;
+        is_there_anything_valuable(raw_content, atag)
+
+
+
+def is_there_anything_valuable(raw_content, atag):
+        pattern = "[^@]+@[^@]+\.[^@]+"
+        x = re.search(pattern, raw_content)
+
+        if x:
+          print(f"YES! We have a match!, {atag['href']}")
+        else:
+          print("No match")
+
+
+
 
 url = "https://pastebin.com/archive"
 
@@ -9,13 +32,16 @@ response = requests.get(url)
 
 html_content = response.text;
 
+parsed_atags = []
+
+
 soup = BeautifulSoup(html_content, "html.parser");
 all_the_atags = soup.find_all("a", class_=False)
 i=0
 for atag in all_the_atags:
      if atag['href'] == '/': break 	
      i += 1
-     if i>3: print(atag)
-		
+     if i>3: check_post(atag)
+
 
 
--- readme.md
+++ readme.md
@@ -6,5 +6,8 @@ Approach:
 -  find all hrefs => which inturn gets us post id's ✅
 -  make a forloop and traverse through all the hrefs ✅
 -  skip the first 3 a tags coz they are basically nav links and skipped when i see an empty href because after that its just footer links.. ✅
--  now we need to scrape individual posts or and get the data 
- 
+-  we have atag which contains href attribute with postID's 
+-  we can use patbin.com/raw/{id} -> url to get the raw data 
+-  take the data and put it thorugh regex -> url to get the raw data
+- build a regex.json or smthing and which will check if the posts has any leaks if yes log it if not do nothing 
+
